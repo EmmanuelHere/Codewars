833e4bf862c5584623dd62a45d6097fa8f6cabcb
Justin H <emmanuelheredia@yahoo.com>
2022-03-30 23:50:21 -0400

    Reversed Strings

---
+++ 3-30 Reversed Strings/main.js
@@ -0,0 +1,17 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str) {
+    return str.split('').reverse().join('');
+}
+
+// const solution = str => str.split('').reverse().join('');
+
+// function solution(s) {
+//     var o = '';
+//     for (var i = s.length - 1; i >= 0; i--)
+//         o += s[i];
+//     return o;
+// }
