2fda192203748b2c349a8db0dacddd2c52b2a2b6
Justin H <emmanuelheredia@yahoo.com>
2022-04-01 17:15:47 -0400

    Even or Odd

---
+++ 4-1 Even or Odd/main.js
@@ -0,0 +1,12 @@
+// Description:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+const even_or_odd = n => (n % 2) ? 'Odd' : 'Even';
+
+// function even_or_odd(number) {
+//     return number % 2 ? "Odd" : "Even"
+// }
+
+// function even_or_odd(number) {
+//     return number % 2 === 0 ? 'Even' : 'Odd';
+// }
