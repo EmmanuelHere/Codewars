24b95e05c097f76614bb24ab628ed3089574bb4f
Justin H <emmanuelheredia@yahoo.com>
2022-04-07 16:59:15 -0400

    Remove String Spaces

---
+++ 4-7 Remove String Spaces/main.js
@@ -0,0 +1,12 @@
+// Description:
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x) {
+    return x.replace(/\s/g, '');
+}
+
+// function noSpace(x) {
+//     return x.split(' ').join('')
+// }
+
+// const noSpace = x => x.split(" ").join("");
