4d5c2b7da0de8100f678fbde895a6d63d9836703
Justin H <emmanuelheredia@yahoo.com>
2022-04-10 23:52:29 -0400

    Add Item to an Array

---
+++ 4-10 Add Item to Array/main.js
@@ -0,0 +1,16 @@
+// Description:
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+
+websites.push('codewars')
+
+// websites[0]="codewars";
+
+// var toAdd = "codewars";
+// if (websites.indexOf(toAdd) === -1) {
+//   websites.push(toAdd);
+// }
