6dd5caa47ed238f3faa15b3df3cf16af39e56d43
Justin H <emmanuelheredia@yahoo.com>
2022-05-09 23:08:01 -0400

    Type of sum

---
+++ 5-9 Type of sum/main.js
@@ -0,0 +1,17 @@
+// Return the type of the sum of the two arguments
+
+const typeOfSum = (a, b) => typeof (a + b);
+
+// function typeOfSum(a, b) {
+//     return typeof (a + b);
+// }
+
+// function typeOfSum(a, b) {
+//     if (typeof a === 'string' || a instanceof String) {
+//         return 'string'
+//     }
+//     if (typeof b === 'string' || b instanceof String) {
+//         return 'string'
+//     }
+//     return 'number'
+// }
