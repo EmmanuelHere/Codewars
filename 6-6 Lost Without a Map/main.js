54a9e936b29f44c1504751d3ee0f75267bbe7636
Justin H <emmanuelheredia@yahoo.com>
2022-06-06 23:27:51 -0400

    Lost Without a Map

---
+++ 6-6 Lost Without a Map/main.js
@@ -0,0 +1,14 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x) {
+    return x.map(n => n * 2);
+}
+
+// maps = x => x.map(e => e * 2);
+
+
+// const maps = arr => arr.map(x => x * 2)
