d6fba2ff98cdfd09c827ee7d19df6749725720d0
Justin H <emmanuelheredia@yahoo.com>
2022-06-08 23:23:54 -0400

    You Can't Code Under Pressure #1

---
+++ 6-9 You Can't Code Under Pressure #1/main.js
@@ -0,0 +1,9 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+const doubleInteger = i => i + i
+
+// function doubleInteger(i) {
+//     return i * 2;
+// }
+
+// const doubleInteger = i => 2 * i;
