16fc27b00f2d8ab4bb725fed187156550cc27a75
Justin H <emmanuelheredia@yahoo.com>
2022-07-07 16:51:45 -0400

    Function 3 - multiplying two numbers

---
+++ 7-7/main.js
@@ -0,0 +1,13 @@
+// Function 3 - multiplying two numbers
+// Implement a function which multiplies two numbers.
+
+let multiply = (a, b) => a * b;
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// function multiply(a, b) {
+//     if (typeof a == 'number' && typeof b == 'number')
+//         return a * b;
+// }
